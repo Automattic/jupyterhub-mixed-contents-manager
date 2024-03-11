2f9084d1a008d0d0267234d59c3a41b7030b325b
Jessie Ross <j@scjr.me>
2024-03-12 00:44:22 +0800

    Simplify

--- src/jupyterhub_mixed_contents_manager/mixed_contents_manager.py
+++ src/jupyterhub_mixed_contents_manager/mixed_contents_manager.py
@@ -6,7 +6,8 @@
 # File adapted from:
 # https://github.com/jupyter/jupyter-drive/blob/master/jupyterdrive/mixednbmanager.py
 
-from typing import Dict
+from typing import Dict, Any
+import copy
 import pathlib
 
 from jupyter_server.services.contents.manager import ContentsManager
@@ -26,6 +27,79 @@ def parse_mount_points_config(conf: str) -> Dict[str, str]:
         }
     else:
         return {}
+
+
+def get_mount_point(mount_points_dict: Dict[str, Any], path: str):
+    return next(
+        mount_point
+        for mount_point in sorted(mount_points_dict.keys(), reverse=True)
+        if pathlib.PurePath(f"/{path}").is_relative_to(
+            pathlib.PurePath(f"/{mount_point}")
+        )
+    )
+
+
+def get_child_path(mount_point: str, path: str):
+    return str(
+        pathlib.PurePath(f"/{path}").relative_to(pathlib.PurePath(f"/{mount_point}"))
+    )
+
+
+def path_lookup(mount_points_managers: Dict[str, Any], path: str):
+    "returns manager, mount_point, child_path"
+    mount_point = get_mount_point(mount_points_managers, path)
+    return (
+        mount_points_managers[mount_point],
+        mount_point,
+        get_child_path(mount_point, path),
+    )
+
+
+def path_dispatch1(method_name):
+    def f(self, path, *args, **kwargs):
+        manager, _mount_point, child_path = self._path_lookup(path)
+        return getattr(manager, method_name)(child_path, *args, **kwargs)
+
+    return f
+
+
+def path_dispatch2(method_name):
+    def f(self, other, path, *args, **kwargs):
+        manager, _mount_point, child_path = self._path_lookup(path)
+        return getattr(manager, method_name)(other, child_path, *args, **kwargs)
+
+    return f
+
+
+def path_dispatch_kwarg(method_name):
+    def f(self, path=""):
+        manager, _mount_point, child_path = self._path_lookup(path)
+        if manager is not None:
+            return getattr(manager, method_name)(path=child_path)
+
+    return f
+
+
+def path_dispatch_rename(method):
+    """
+    decorator for rename-like function, that need dispatch on 2 arguments
+    """
+
+    def f(self, path_a, path_b):
+        manager_a, mount_point_a, child_path_a = self._path_lookup(path_a)
+        manager_b, mount_point_b, child_path_b = self._path_lookup(path_b)
+
+        if mount_point_a != mount_point_b:
+            raise ValueError(
+                "Does not know how to move things across contents manager mountpoints"
+            )
+
+        if manager_a is not None:
+            return getattr(manager_a, method.__name__)(child_path_a, child_path_b)
+        else:
+            return method(self, path_a, path_b)
+
+    return f
 
 
 class MixedContentsManager(ContentsManager):
@@ -51,169 +125,73 @@ class MixedContentsManager(ContentsManager):
             ).items()
         }
         assert self.path_lookup("")[1] == ""
-        assert self.mount_points_managers[""]
+        assert self.mount_points_managers[""], "Root mount point required"
         assert self.mount_points_managers["hdfs-home"]
         assert self.get("")
 
-    def get_mount_point(self, path: str):
-        return next(
-            (
-                mount_point
-                for mount_point in sorted(
-                    self.mount_points_managers.keys(), reverse=True
-                )
-                if pathlib.PurePath(f"/{path}").is_relative_to(
-                    pathlib.PurePath(f"/{mount_point}")
-                )
-            ),
-            None,
-        )
-
-    def get_child_path(self, mount_point: str, path: str):
-        return str(
-            pathlib.PurePath(f"/{path}").relative_to(
-                pathlib.PurePath(f"/{mount_point}")
-            )
-        )
-
-    def path_lookup(self, path: str):
-        "returns manager, mount_point, child_path"
-        mount_point = self.get_mount_point(path)
-        if mount_point is not None:
-            return (
-                self.mount_points_managers.get(mount_point),
-                mount_point,
-                self.get_child_path(mount_point, path),
-            )
-        return None, None, None
-
-    def path_dispatch1(method):
-        def _wrapper_method(self, path, *args, **kwargs):
-            manager, _mount_point, child_path = self.path_lookup(path)
-            if manager is not None:
-                return getattr(manager, method.__name__)(child_path, *args, **kwargs)
-            else:
-                return method(self, path, *args, **kwargs)
-
-        return _wrapper_method
-
-    def path_dispatch2(method):
-        def _wrapper_method(self, other, path, *args, **kwargs):
-            manager, _mount_point, child_path = self.path_lookup(path)
-            if manager is not None:
-                return getattr(manager, method.__name__)(
-                    other, child_path, *args, **kwargs
-                )
-            else:
-                return method(self, other, path, *args, **kwargs)
-
-        return _wrapper_method
-
-    def path_dispatch_kwarg(method):
-        def _wrapper_method(self, path=""):
-            manager, _mount_point, child_path = self.path_lookup(path)
-            if manager is not None:
-                return getattr(manager, method.__name__)(path=child_path)
-            else:
-                return method(self, path=path)
-
-        return _wrapper_method
-
-    def path_dispatch_rename(method):
-        """
-        decorator for rename-like function, that need dispatch on 2 arguments
-        """
-
-        def _wrapper_method(self, path_a, path_b):
-            manager_a, mount_point_a, child_path_a = self.path_lookup(path_a)
-            manager_b, mount_point_b, child_path_b = self.path_lookup(path_b)
-
-            if mount_point_a != mount_point_b:
-                raise ValueError(
-                    "Does not know how to move things across contents manager mountpoints"
-                )
-
-            if manager_a is not None:
-                return getattr(manager_a, method.__name__)(child_path_a, child_path_b)
-            else:
-                return method(self, path_a, path_b)
-
-        return _wrapper_method
+    def _path_lookup(self, path: str):
+        return path_lookup(self.mount_point_managers, path)
 
     @path_dispatch1
     def dir_exists(self, path):
-        if len(path) == 0 or path in self.mount_points_managers.keys():
-            return True
-        return False
+        raise NotImplementedError()
 
     @path_dispatch1
     def is_hidden(self, path):
-        if (len(path) == 0) or path in self.mount_points_managers.keys():
-            return False
-        raise NotImplementedError("...." + path)
+        raise NotImplementedError()
 
     @path_dispatch_kwarg
     def file_exists(self, path=""):
-        if len(path) == 0:
-            return False
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch1
     def exists(self, path):
-        if len(path) == 0:
-            return True
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch1
     def get(self, path, **kwargs):
-        if len(path) == 0:
-            return [{"type": "directory"}]
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch2
     def save(self, model, path):
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     def update(self, model, path):
-        manager_a, mount_point_a, child_path_a = self.path_lookup(path)
-        _manager_b, mount_point_b, child_path_b = self.path_lookup(model["path"])
+        manager_a, mount_point_a, child_path_a = self._path_lookup(path)
+        _manager_b, mount_point_b, child_path_b = self._path_lookup(model["path"])
 
         if mount_point_a != mount_point_b:
             raise ValueError("Cannot move files across mount points")
 
-        model["path"] = child_path_b
+        new_model = copy.deepcopy(model)
+        new_model["path"] = child_path_b
 
-        if manager_a is not None:
-            return getattr(manager_a, "update")(model, child_path_a)
-        else:
-            return self.method(model, path)
+        return getattr(manager_a, "update")(model, child_path_a)
 
     @path_dispatch1
     def delete(self, path):
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch1
     def create_checkpoint(self, path):
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch1
     def list_checkpoints(self, path):
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch2
     def restore_checkpoint(self, checkpoint_id, path):
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch2
     def delete_checkpoint(self, checkpoint_id, path):
-        raise NotImplementedError("NotImplementedError")
+        raise NotImplementedError()
 
     @path_dispatch_rename
     def rename_file(self, old_path, new_path):
-        """Rename a file."""
-        raise NotImplementedError("must be implemented in a subclass")
+        raise NotImplementedError()
 
     @path_dispatch_rename
     def rename(self, old_path, new_path):
-        """Rename a file."""
-        raise NotImplementedError("must be implemented in a subclass")
+        raise NotImplementedError()
